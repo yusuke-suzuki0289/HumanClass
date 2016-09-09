7b272d74b623aa7471905d887ac75d1363814d8b
yusuke-suzuki0289 <yusu.suz.0320@gmail.com>
2016-09-09 16:25:07 +0900

    Add files via upload

--- app/src/main/java/jp/techacademy/yusuke2/suzuki/javalog/Animal.java
+++ app/src/main/java/jp/techacademy/yusuke2/suzuki/javalog/Animal.java
@@ -7,7 +7,7 @@ abstract class Animal {
     // メンバ変数
     String name;  // 名前
     int age;    // 年齢
-    String hobby; //趣味
+//    String hobby; //趣味（←レビュー指摘により、animalクラスには保持しない）
 
     abstract public void say();
 }
